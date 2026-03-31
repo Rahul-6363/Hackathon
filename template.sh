2c19bef65bbcfef2b7d0e3a43722ced2743f86c2
Rahul P <rrahulrahul220@gmail.com>
2026-03-31 16:19:02 +0530

    folders and files created

---
+++ template.sh
@@ -0,0 +1,46 @@
+
+mkdir backend
+mkdir frontend
+
+mkdir backend/app
+mkdir backend/app/routes
+mkdir backend/app/models
+mkdir backend/app/services
+mkdir frontend/public
+mkdir frontend/src/
+mkdir frontend/src/components
+mkdir frontend/src/pages
+mkdir frontend/src/services
+
+
+touch backend/app/main.py
+touch backend/app/routes/auth.py
+touch backend/app/routes/chatbot.py
+touch backend/app/routes/health.py
+
+
+touch backend/app/models/user.py
+touch backend/app/services/ai_service.py
+touch backend/app/config.py
+touch backend/requirements.txt
+touch backend/run.py
+
+
+touch frontend/src/components/Navbar.js
+touch frontend/src/components/Chatbot.js
+touch frontend/src/components/Footer.js
+
+
+touch frontend/src/pages/Home.js
+touch frontend/src/pages/Login.js
+touch frontend/src/pages/Dashboard.js
+
+touch frontend/src/services/api.js
+
+touch frontend/src/App.js
+touch frontend/src/index.js
+touch frontend/src/App.css
+
+touch frontend/package.json
+touch frontend/.env
+
